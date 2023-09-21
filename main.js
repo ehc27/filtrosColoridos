f7c0effb9ee2a989c28e43143d4fb3f8cfad78d3
ehc27 <118471105+ehc27@users.noreply.github.com>
2023-09-21 14:43:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload() {}
+function setup(){
+    canvas = createCanvas(640,480)
+    canvas.position(110,250)
+    video = createCapture(VIDEO)
+    tint_color = ""
+    video.hide()
+}
+function draw(){
+    image(video,0,0,640,480)
+    tint(tint_color)
+}
+function take_snapshot(){
+    save("selfie.png")
+}
+function filter_tint(){
+    tint_color = document.getElementById("input").value
+}
